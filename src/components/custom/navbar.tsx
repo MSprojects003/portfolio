8ade0f9e43c608d0f0f8d01624c3f2df06602ee6
MSprojects003 <mbaasatheef@gmail.com>
2025-08-21 18:15:13 +0530

    cv added

--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { User,  Folder,  Paintbrush, Paperclip } from 'lucide-react';
-import cv from "../../assets/cv/FATHIMA SHAREENA my new cv.pdf";
+import cv from "../../assets/cv/FATHIMA SHAREENA new 1 (1).pdf";
 const Navbar: React.FC = () => {
   const navItems = [
     { name: 'Profile', href: '#profile', icon: User },
